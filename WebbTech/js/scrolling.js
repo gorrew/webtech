5c0f30388c2eb9db738d951037c550715d4650bf
gorrew <gordon.won86@gmail.com>
2016-03-16 14:01:27 +0100

    first checkin

---
+++ WebbTech/js/scrolling.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    //Bootstrap scrollspy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 160
+    });
+
+    // smooth scrolling
+    $('nav a, .down-btn a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 110
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // parallax scrolling with stellar.js
+    $(window).stellar();
+
+
+});
